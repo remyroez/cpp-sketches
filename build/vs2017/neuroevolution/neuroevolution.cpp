c61b5168756f1e8a45b4c366e2cb03c2bb54a2ed
Remy Roez <remyroez+github@gmail.com>
2017-08-24 22:18:58 +0900

    Neuroevolution を追加

---
+++ build/vs2017/neuroevolution/neuroevolution.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+#include "genetic_algorithm/genetic_algorithm.hpp"
+#include "neural_network/neural_network.hpp"
+
+namespace ga = genetic_algorithm;
+namespace nn = neural_network;
+
+namespace {
+
+
+
+} // namespace
+
+int main()
+{
+
+
+    return 0;
+}
+
